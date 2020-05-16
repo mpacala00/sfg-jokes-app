87b2c4fd4b21271e418b6c77a51084aca3a4986d
mpacala00 <mpacala00@gmail.com>
2020-05-16 14:39:59 +0200

    Example of XML configuration.

--- src/main/java/com/mpacala/jokesapp/JokesappApplication.java
+++ src/main/java/com/mpacala/jokesapp/JokesappApplication.java
@@ -2,8 +2,10 @@ package com.mpacala.jokesapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("classpath:jokesapp-config.xml")
 public class JokesappApplication {
 
 	public static void main(String[] args) {
